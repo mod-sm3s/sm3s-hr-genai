9682fba751382a0e44c98916115bb057574445b5
mod-sm3s <mohammaddiab@smartera3s.com>
2025-04-08 11:46:39 +0200

    Update job_description.py

--- job_description.py
+++ job_description.py
@@ -7,7 +7,7 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.7)
 # Job Description Generator
 def generate_job_description(title, industry="", responsibilities="", skills="", experience=""):
     base_prompt = f"Generate a professional job description for a {title} position."
-
+    base_prompt += "Company name is Smartera"
     if industry:
         base_prompt += f" The industry is {industry}."
     if responsibilities:
