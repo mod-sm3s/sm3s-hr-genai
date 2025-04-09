94272c2bc24a4acc16edd449a3b320e837876b66
mod-sm3s <mohammaddiab@smartera3s.com>
2025-04-09 14:43:08 +0200

    Update job_description.py

--- job_description.py
+++ job_description.py
@@ -9,10 +9,10 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.7)
 # # Job Description Generator
 def generate_job_description(title, industry="", responsibilities="", skills="", experience=""):
     base_prompt = f"Generate a professional job description for a {title} position."
-    base_prompt += "make font color of sections headers like About , Job Summary , Responsibilities to be blue  , do not generate html file , just normal file"
+    base_prompt += "make font color of sections headers like About , Job Summary and Responsibilities to be blue  , do not generate html file , just normal file"
     base_prompt += "Return **only the formatted job description** — no explanation or intro text. Do NOT include phrases like 'Here’s a job description for...' etc."
     base_prompt += "Do not return the Company name Smartera at the beginning of the result"
-   # base_prompt += "Do not write font type or size in result"
+    base_prompt += "Do not write font type or size in result"
     if industry:
         base_prompt += f" The industry is {industry}."
     if responsibilities:
