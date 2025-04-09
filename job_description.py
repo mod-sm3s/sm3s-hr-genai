6fb0572846dd06a71361b9c5c94a711c3a93f19d
mod-sm3s <mohammaddiab@smartera3s.com>
2025-04-09 14:53:40 +0200

    Update job_description.py

--- job_description.py
+++ job_description.py
@@ -13,7 +13,7 @@ def generate_job_description(title, industry="", responsibilities="", skills="",
     base_prompt += "Return **only the formatted job description** — no explanation or intro text. Do NOT include phrases like 'Here’s a job description for...' etc."
     base_prompt += "Do not return the Company name Smartera at the beginning of the result"
     #base_prompt += "Do not generate any html elements , "
-    base_prompt += "Format the job description using basic HTML with section headers like "<h2 style='color:blue'>About</h2>. Use Arial font. Return only the HTML content."
+    base_prompt += "Format the job description using basic HTML with section headers like '<h2 style='color:blue'>About</h2>. Use Arial font. Return only the HTML content."
 
     if industry:
         base_prompt += f" The industry is {industry}."
