2b09b0885044253a23e38a83c18d6202bbab306d
mod-sm3s <mohammaddiab@smartera3s.com>
2025-04-09 14:01:29 +0200

    Update job_description.py

--- job_description.py
+++ job_description.py
@@ -54,6 +54,7 @@ def render_paragraph(pdf, text):
 
 def generate_job_pdf(text, filename="job_description.pdf"):
     pdf = StyledPDF()
+    pdf.add_page()
     pdf.add_font('DejaVu', '', 'fonts/DejaVuSans.ttf', uni=True)
 
     # Extract and render by section
